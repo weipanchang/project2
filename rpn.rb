766471064b230705a5a7ca6ce5916178b93748ca
wchang <weipanchang@hotmail.com>
2014-02-02 15:32:50 -0800

    convert python to ruby

--- rpn.rb
+++ rpn.rb
@@ -3,30 +3,36 @@
 #import sys
 #
 def is_op(c)
-    return if c in ['*', '/', '+', '-']
+    return ['*', '/', '+', '-'].include? c
 end
 
-#def do_op(lhs, op, rhs):
-#    if op == '*': return lhs * rhs
-#    elif op == '/': return lhs / rhs
-#    elif op == '+': return lhs + rhs
-#    elif op == '-': return lhs - rhs
+def do_op(lhs, op, rhs)
+    case
+    when op == '*' then return lhs * rhs
+    when op == '/' then return lhs / rhs 
+    when op == '+' then return lhs + rhs
+    when op == '-' then return lhs - rhs
+    end
+
+def rpn(tokens)
+    stack = Array.new[]
 #
-#def rpn(tokens):
-#    stack = []
-#
-#    for t in tokens:
-#        if is_op(t):
-#            lhs = stack.pop()
-#            rhs = stack.pop()
-#
-#            result = do_op(lhs, t, rhs)
-#            stack.append(result)
-#        else:
-#            stack.append(float(t))
-#
-#    if len(stack) == 1:
-#        return stack[0]
-#
+    tokens.each do |t|
+        if is_op(t) then
+            lhs = stack.shift()
+            rhs = stack.shift()
+
+            result = do_op(lhs, t, rhs)
+            stack.push(result)
+        else
+            stack.push(float(t))
+        end
+    end
+    if stack.size == 1 then
+        return stack[0]
+    end
+end
+
+puts rpn('2', '3', '+')
 #if __name__ == '__main__':
 #    print rpn(sys.argv[1:])
