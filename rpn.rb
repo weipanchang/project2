a164e52e9e37620f38fee58cf9c2e351c056b410
wchang <weipanchang@hotmail.com>
2014-02-02 16:03:29 -0800

    add read from command line

--- rpn.rb
+++ rpn.rb
@@ -13,26 +13,27 @@ def do_op(lhs, op, rhs)
     when op == '+' then return lhs + rhs
     when op == '-' then return lhs - rhs
     end
+end
 
 def rpn(tokens)
-    stack = Array.new[]
-#
+    stack = []
     tokens.each do |t|
         if is_op(t) then
             lhs = stack.shift()
             rhs = stack.shift()
-
             result = do_op(lhs, t, rhs)
             stack.push(result)
         else
-            stack.push(float(t))
+            stack.push((t.to_f))
         end
     end
     if stack.size == 1 then
-        return stack[0]
+        return stack.shift()
     end
+    
 end
 
-puts rpn('2', '3', '+')
-#if __name__ == '__main__':
-#    print rpn(sys.argv[1:])
+if __FILE__ == $0 then
+    puts rpn(ARGV)
+end
+
